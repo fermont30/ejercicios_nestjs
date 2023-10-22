969b7605dc2ee13d3a28973ed4a56adec3da8464
fermont30 <lfq.montoya@yavirac.edu.ec>
2023-10-22 18:30:33 -0500

    first commit

---
+++ src/entities/product.entity.ts
@@ -0,0 +1,58 @@
+import {Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import { CategoryEntity } from "./category.entity";
+
+@Entity('products',{schema:'ventas'})
+
+export class ProductEntity
+{
+@PrimaryGeneratedColumn('uuid')
+id :string;
+@CreateDateColumn({
+    name:'create_at',
+    type:'timestamp',
+    default:() => 'CURRENT_TIMESTAMP'
+})
+createAT:Date;
+
+@UpdateDateColumn({
+    name:'update_at',
+    type:'timestamp',
+    default:() => 'CURRENT_TIMESTAMP'
+})
+updateAT:Date;
+
+@DeleteDateColumn({
+    name:'delete_at',
+    type:'timestamp',
+    nullable: true,
+})
+deleteAT:Date;
+
+@ManyToMany(()=> CategoryEntity, category => category.products)
+category:CategoryEntity
+
+@Column('varchar',{
+    name:'title',
+    nullable:false,
+    comment:'nombre del producto'   
+})
+title:string;
+@Column('number',{
+    name:'price',
+    nullable:false,
+    comment:'precio del producto'   
+})
+price:number;
+
+@Column('varchar',{
+    name:'description',
+    nullable:true,
+    comment:'descipcion del producto'   
+})
+description:string;
+
+
+
+}
+
+
